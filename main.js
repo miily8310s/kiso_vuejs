2fb727fca2d9fc569326edc7ae434cad34193315
miily8310s <42486288+miily8310s@users.noreply.github.com>
2018-10-20 22:04:04 +0900

    main.jsをコミット

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        message:'Hello Vue.js',
+        list:['りんご','ばなな','いちご'],
+        show:true
+    },
+    methods:{
+        handleClick: function(event){
+            alert(event.target)
+        }
+    }
+})
