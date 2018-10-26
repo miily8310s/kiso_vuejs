205c71fe0d1b97f537b4473dfb75616713c60221
miily8310s <42486288+miily8310s@users.noreply.github.com>
2018-10-26 21:59:48 +0900

    ch6にcssとjsファイルをコミット

---
+++ ch6/main.js
@@ -0,0 +1,28 @@
+// SVGパーツのコンポーネントを定義
+Vue.component('my-circle', {
+    template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
+    props: {
+        fill: String
+    }
+})
+var app = new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        order: false,
+        list: [
+            { id: 1, name: 'りんご', price: 100 },
+            { id: 2, name: 'ばなな', price: 200 },
+            { id: 3, name: 'いちご', price: 300 }
+        ],
+        toggle: false
+    },
+    computed: {
+        sortedList: function () {
+            return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+        },
+        fill: function(){
+            return this.toggle ? 'lightpink' : 'skyblue'
+        }
+    }
+})
